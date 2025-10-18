646fd18e5016e2dd75ddb8c772432c946f664772
Arpit Alkari <71004336+DK004@users.noreply.github.com>
2025-10-19 00:26:39 +0530

    final update
    just paste the code any how and press enter and boom course is complete

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 var inputs = document.getElementsByClassName('ud-real-toggle-input');
+var panels = document.getElementsByClassName('ud-accordion-panel-toggler');
+for(var i = 0; i < panels.length; i++) {
+      var toggleId = panels[i].getAttribute('data-css-toggle-id');
+  if (toggleId) {
+        var statusSpan = document.getElementById(toggleId);
+        if (statusSpan && !statusSpan.hasAttribute('data-checked')) {
+           if(statusSpan.getAttribute('data-checked')==='checked'){
+           continue;
+           }else{
+               panels[i].click();
+           }
+        } 
+   
+}
+}
 for(var i = 0; i < inputs.length; i++) {
-    inputs[i].disabled = false;
-    inputs[i].click();
+    const ariaLabel = inputs[i].getAttribute('aria-label');
+    if (ariaLabel && ariaLabel.includes("complete") && !ariaLabel.includes("incomplete")) {
+        inputs[i].disabled = false;
+        inputs[i].click();
+    }
+    
 }
