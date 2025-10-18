6e55a9ea80dbd5ef1007aff99aad3d231b96a10b
Arpit Alkari <71004336+DK004@users.noreply.github.com>
2025-10-19 00:34:45 +0530

    Change condition to check for 'data-checked'

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ for(var i = 0; i < panels.length; i++) {
       var toggleId = panels[i].getAttribute('data-css-toggle-id');
   if (toggleId) {
         var statusSpan = document.getElementById(toggleId);
-        if (statusSpan && !statusSpan.hasAttribute('data-checked')) {
+        if (statusSpan && statusSpan.hasAttribute('data-checked')) {
            if(statusSpan.getAttribute('data-checked')==='checked'){
            continue;
            }else{
                panels[i].click();
            }
         } 
-   
-}
+  }
 }
 for(var i = 0; i < inputs.length; i++) {
     const ariaLabel = inputs[i].getAttribute('aria-label');
